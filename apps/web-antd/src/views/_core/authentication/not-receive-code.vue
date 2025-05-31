3ef732532b8344e72d2dba327977b0ce7db5845e
jonnzer <janebenjohn@outlook.com>
2025-05-31 18:28:00 +0800

    fix(dev): 更新全局配置，修改登录

---
+++ apps/web-antd/src/views/_core/authentication/not-receive-code.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+
+// const emit = defineEmits(['blur', 'change']);
+import { Button } from 'ant-design-vue';
+
+defineOptions({ name: 'NotReceiveCode', components: { AButton: Button } });
+
+const handleClick = () => {
+  console.warn('Button clicked');
+};
+
+onMounted(() => {
+  console.error('NotReceiveCode component mounted');
+});
+</script>
+<template>
+  <div class="-mb-1 -mt-1 flex w-full justify-end gap-1">
+    <a-button class="not-receive-wrapper" type="text" @click="handleClick">
+      收不到验证码？
+    </a-button>
+  </div>
+</template>
+<style scoped lang="scss">
+.not-receive-wrapper {
+  color: hsl(227, 13%, 57%);
+  font-size: 14px;
+  padding: 0;
+  height: auto;
+  cursor: pointer;
+  &:hover {
+    background-color: unset;
+  }
+}
+</style>
